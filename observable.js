b8c7a0b0eb0ee2e255c3cfea0f6902647a9b43e7
DevGHalid <47138142+DevGHalid@users.noreply.github.com>
2020-02-17 23:35:39 +0300

    Update observable.js

--- observable.js
+++ observable.js
@@ -1,71 +1,100 @@
 export default function observable(data) {
-  const events = {};
+ const events = {};
 
-  function has(name) {
-    return data.hasOwnProperty(name);
+ /**
+  * @param {string} name
+  * @returns {boolean}
+  */
+ function has(name) {
+  return data.hasOwnProperty(name);
+ }
+
+ /**
+  * @param {string} name
+  * @returns {boolean}
+  */
+ function hasEvent(name) {
+  return events.hasOwnProperty(name);
+ }
+
+ /**
+  * @param {string} name
+  * @param {Function} listener
+  * @returns {void}
+  */
+ function subscribe(name, listener) {
+  if (typeof listener !== 'function') {
+   throw new TypeError('The second argument must be a function');
   }
 
-  function hasEvent(name) {
-    return events.hasOwnProperty(name);
+  if (!hasEvent(name)) {
+   events[name] = [];
   }
+  events[name].push(listener);
+ }
 
-  function subscribe(name, listener) {
-    if (typeof listener !== 'function') {
-      throw new TypeError('The second argument must be a function');
-    }
+ /**
+  * @param {string} name
+  * @returns {void}
+  */
+ function unsubscribe(name) {
+  events[name] = [];
+ }
 
-    if (!hasEvent(name)) {
-      events[name] = [];
-    }
-    events[name].push(listener);
-  }
-
-  function unsubscribe(name) {
-    events[name] = [];
-  }
-
-  function notify(listener) {
-    if (typeof listener !== 'function') {
-      throw new TypeError('The second argument must be a function');
-    }
-
-    for (let name in data) {
-      if (!hasEvent(name)) {
-        events[name] = [];
-      }
-
-      events[name].push(listener);
-    }
-  }
-
-  function call(name) {
-    if (has(name) && hasEvent(name)) {
-      const listeners = events[name];
-
-      for (let listener of listeners) {
-        listener();
-      }
-    }
-  }
-
-  function makeReactive(name, value) {
-    Object.defineProperty(data, name, {
-      get() {
-        return value;
-      },
-      set(newValue) {
-        if (value !== newValue) {
-          value = newValue;
-
-          call(name);
-        }
-      }
-    });
+ /**
+  * @param {Function} listener
+  * @returns {void}
+  */
+ function notify(listener) {
+  if (typeof listener !== 'function') {
+   throw new TypeError('The second argument must be a function');
   }
 
   for (let name in data) {
-    makeReactive(name, data[name]);
-  }
+   if (!hasEvent(name)) {
+    events[name] = [];
+   }
 
-  return { data, has, subscribe, unsubscribe, notify, call, makeReactive };
+   events[name].push(listener);
+  }
+ }
+
+ /**
+  * @param {string} name
+  * @returns {void}
+  */
+ function call(name) {
+  if (has(name) && hasEvent(name)) {
+   const listeners = events[name];
+
+   for (let listener of listeners) {
+    listener();
+   }
+  }
+ }
+
+ /**
+  * @param {string} name
+  * @param {void} value
+  */
+ function makeReactive(name, value) {
+  Object.defineProperty(data, name, {
+   get() {
+    return value;
+   },
+   set(newValue) {
+    if (value !== newValue) {
+     value = newValue;
+
+     call(name);
+    }
+   }
+  });
+ }
+
+ for (let name in data) {
+  makeReactive(name, data[name]);
+ }
+
+ return { data, has, subscribe, unsubscribe, notify, call, makeReactive };
 }
